ee3f63908237c3dc31a23565f58ed769af5de252
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-03-19 23:48:26 +0200

    feat(lib2): change lib2

---
+++ .changeset/brave-stingrays-chew.md
@@ -0,0 +1,5 @@
+---
+"@nikosdouvlis/lib2": patch
+---
+
+Change lib2 log
--- packages/lib2/src/index.ts
+++ packages/lib2/src/index.ts
@@ -1,3 +1,5 @@
 export const helperFromLib2 = (name: string = 'lib2') => {
+    console.log('hello 2' +
+        '')
     console.log('hello from ' + name)
 }
