0b19caf1b1d32c6933eb7a10733a4ddcdd35f900
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-03-19 22:25:31 +0200

    feat(lib1): add a new log to the helper

---
+++ .changeset/fluffy-numbers-poke.md
@@ -0,0 +1,5 @@
+---
+"@nikosdouvlis/lib1": patch
+---
+
+Add a new log to the lib1 helper for fun
--- packages/lib1/src/index.ts
+++ packages/lib1/src/index.ts
@@ -1,3 +1,4 @@
 export const helperFromLib1 = (name: string = 'lib1') => {
+    console.log('this is a change!');
     console.log('hello from ' + name)
 }
