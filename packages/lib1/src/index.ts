9ec61db5475d41ef89de1af69f15906605acf373
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-03-20 14:47:13 +0200

    feat(lib1): Change

--- packages/lib1/src/index.ts
+++ packages/lib1/src/index.ts
@@ -1,4 +1,4 @@
 export const helperFromLib1 = (name: string = 'lib1') => {
-    console.log('this is a change! 5');
+    console.log('this is a change! 6');
     console.log('hello from ' + name)
 }
