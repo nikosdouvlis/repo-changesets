501cc4fb6b23b0222b4939a18445333cacc271dd
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-04-12 10:06:17 +0300

    lib1 change

--- scripts/snapshot.mjs
+++ scripts/snapshot.mjs
@@ -6,5 +6,12 @@ import {constants} from "./common.mjs";
 const prefix = argv.name || argv._[0] || 'snap';
 
 await $`npx json -I -f ${constants.ChangesetConfigFile} -e "this.changelog = false"`;
-await $`npx changeset version --snapshot ${prefix}`;
+
+const res = await $`npx changeset version --snapshot ${prefix}`;
+const noChanges = res.stderr.includes("No unreleased changesets found");
+
 await $`git checkout HEAD -- ${constants.ChangesetConfigFile}`;
+
+if (noChanges) {
+    await $`exit 1`
+}
