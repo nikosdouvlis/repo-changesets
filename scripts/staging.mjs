d457cce7764a66155bbf8fdafe8b028822e9c0db
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-04-12 09:50:48 +0300

    lib1 change

--- scripts/staging.mjs
+++ scripts/staging.mjs
@@ -4,5 +4,12 @@ import 'zx/globals'
 import {constants} from "./common.mjs";
 
 await $`npx json -I -f ${constants.ChangesetConfigFile} -e "this.changelog = false"`;
-await $`npx changeset version --snapshot staging`;
+
+const res = await $`npx changeset version --snapshot staging`;
+const noChanges = res.stderr.includes("No unreleased changesets found");
+
 await $`git checkout HEAD -- ${constants.ChangesetConfigFile}`;
+
+if (noChanges) {
+    await $`exit 1`
+}
